731513ccd21269a28472432b2a90f6ef94b5854d
SHIFU <tomer5142200@walla.co.il>
2026-05-20 10:45:12 +0300

    fix get methood

--- server.ts
+++ server.ts
@@ -256,6 +256,50 @@ app.post("/sign-in", async (req: Request, res: Response) => {
     });
   }
 });
+///////////////////////////////////////////////////
+//dashboard
+//////////////////////////////////////////////////
+app.get("/dashboard", async (req: Request, res: Response) => {
+  try {
+    const authHeader = req.headers.authorization;
+
+    if (!authHeader || !authHeader.startsWith("Bearer ")) {
+      return res.status(401).json({
+        success: false,
+        message: "No token",
+      });
+    }
+
+    const token = authHeader.split(" ")[1];
+
+    const decoded = jwt.verify(token, process.env.JWT_SECRET as string) as {
+      id: string;
+    };
+
+    const user = await User.findById(decoded.id);
+
+    if (!user) {
+      return res.status(404).json({
+        success: false,
+        message: "User not found",
+      });
+    }
+
+    res.status(200).json({
+      success: true,
+      data: {
+        id: user._id,
+        name: user.name,
+        email: user.email,
+      },
+    });
+  } catch (error) {
+    res.status(401).json({
+      success: false,
+      message: "Invalid token",
+    });
+  }
+});
 /////////////////////////////////////////////////
 //reveiw page
 ////////////////////////////////////////////////
